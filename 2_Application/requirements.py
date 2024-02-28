df77f15dc2270d47361f4060c8f04df3af77bcd0
kenza-benkirane <kenzabenkirane12@gmail.com>
2024-02-28 11:35:10 +0000

    update

---
+++ 2_Application/requirements.py
@@ -0,0 +1,40 @@
+
+
+###############################################################################
+#               This Code imports all the packages, creates all the functions needed all along the Database creating - updating
+#########################################################################################
+from numpy import *
+import tcl
+from time import strftime
+import os;import webbrowser
+from pathlib import Path;
+import datetime;
+import pandas as pd;from pandastable import Table,TableModel;
+from matplotlib import style;
+# BASIC
+import subprocess;
+import tkinter as tk;from tkinter import ttk;from tkinter.ttk import Combobox;
+import smtplib;
+from datetime import date
+from datetime import datetime
+from openpyxl import Workbook, load_workbook
+today=date.today();now=datetime.now()
+Actual_datetime=strftime('%B %d, %Y %I:%M:%S %p')
+from tkinter import *
+from tkinter.ttk import Combobox
+from pathlib import Path
+import pandas as pd
+import datetime
+from pandastable import Table,TableModel
+#import pandas.io.data as plt
+from matplotlib import style
+
+# STYLE SETTING
+#style.use('ggplt')
+bg_color='#EEF9FF'
+white_bg='#FFFFFF'
+
+# GUI
+import tkinter as tk
+from tkinter import ttk
+import smtplib
